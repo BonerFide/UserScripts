489c0c58804c427092869e2416555828f5922c64
BonerFide <57783364+BonerFide@users.noreply.github.com>
2025-05-07 17:16:08 +1000

    Update StashVideoDataType.js

--- scripts/StashVideoDataType.js
+++ scripts/StashVideoDataType.js
@@ -1,10 +1,11 @@
 /** Class representing the metadata of a Video, provides a standard type for scraping into Stash */
 class VideoData{
-    constructor({title = "", date = Date(0), tags = [], performers = [], url = "", studio = "", details = "", image = "",id = 0, dlKey = "", dlUrl = "", downloaded = false, counter = 0, source = "Def_AutoDLv1.2"}){
+    constructor({title = "", date = Date(0), tags = [], performers = [], url = "", site = "", studio = "", details = "", image = "",id = 0, dlKey = "", dlUrl = "", downloaded = false, counter = 0, source = "Def_AutoDLv1.2"}){
         this.title = title
         this.date = date
         this.tags = tags
         this.url = url
+        this.site = site
         this.studio = studio
         this.details = details
         this.image = image
@@ -29,21 +30,25 @@ class VideoData{
      * @param {string} type - File extension
      * @returns {string} - Filename based on the video's data
      */
-    toFileName(studio = true, id = true, title = true, type = ".mp4", counter = false){
+    toFileName(studio = true, id = true, title = true, type = ".mp4", counter = false, site = false){
          let elements = []
+         
+         if (site) {
+            elements.push(this.site)
+         }
          if(studio){
             elements.push(this.studio)
-         }
-         if(id){
-            elements.push(this.id)
          }
          if(title){
             elements.push(this.title)
          }
+        if(id){
+            elements.push(this.id)
+        }
         if(counter && this.counter > 0){
             elements.push(this.counter)
         }
-        let filename = elements.join(' - ') + type
+        let filename = elements.join('.') + '_' + this.id + type
         filename = filename.replace(/[/\\?%*:|"<>]/g, '-');
         return filename
     }
